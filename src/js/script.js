10d70f0986050ae76c76ea374a3cdfc17291a7c1
GiuliaStella <ggiulia757@gmail.com>
2026-05-13 15:31:59 -0300

    adicionando slidehow para o como usar pois as imagens do prototipo estao no quero testar, e fazendo a animacao de bolinhas sem biblioteca

--- src/js/script.js
+++ src/js/script.js
@@ -8,6 +8,7 @@ botao.addEventListener('submit', function (event) {
     const nome = document.getElementById("nome").value.trim();
     const sobrenome = document.getElementById("sobrenome").value.trim();
     const email = document.getElementById("email").value.trim();
+    const senha = document.getElementById("senha").value.trim();
 
     // Validações básicas do formulário
     if (!nome || !sobrenome || !email) {
@@ -19,8 +20,12 @@ botao.addEventListener('submit', function (event) {
         alert("E-mail inválido!");
         return; // Parar o código aqui se o e-mail estiver errado
     }
+    if (senha.length < 6) {
+    alert("Senha muito curta!");
+    return;
+}
 
-    alert("Cadastro realizado com sucesso! Bem-vindo, " + nome + "!"); });
+    alert("Cadastro realizado com sucesso! Bem-vindo, " + nome + "!");
 
      //Exibindo tudo no console de forma organizada
     console.log("==============================");
@@ -28,3 +33,79 @@ botao.addEventListener('submit', function (event) {
     console.log("==============================");
     console.log(`👤 Nome Completo: ${nome} ${sobrenome}`);
     console.log(`📧 E-mail:        ${email}`);
+});
+
+
+// fazendo o slide how dos passos de como funciona
+
+const slidesPassos = document.querySelectorAll(".slide-passo");
+const btnNextPasso = document.querySelector(".next-passo");
+const btnPrevPasso = document.querySelector(".prev-passo");
+
+let passoAtual = 0;
+
+function mostrarPasso(index) {
+    slidesPassos.forEach(slide => {
+        slide.classList.remove("ativo-passo");
+    });
+    slidesPassos[index].classList.add("ativo-passo");
+}
+
+/* próximo botao */
+
+btnNextPasso.addEventListener("click", () => {
+    passoAtual++;
+    if (passoAtual >= slidesPassos.length) {
+        passoAtual = 0;
+    }
+    mostrarPasso(passoAtual);
+});
+
+/* botão anterior */
+
+btnPrevPasso.addEventListener("click", () => {
+    passoAtual--;
+    if (passoAtual < 0) {
+        passoAtual = slidesPassos.length - 1;
+    }
+    mostrarPasso(passoAtual);
+});
+
+/* troca automática */
+
+setInterval(() => {
+    passoAtual++;
+    if (passoAtual >= slidesPassos.length) {
+        passoAtual = 0;
+    }
+    mostrarPasso(passoAtual);
+}, 4000);
+
+
+
+
+
+
+// animacao de bolinhas do hero, funcionalidades e como funciona sem biblioteca
+
+const particlesContainers = document.querySelectorAll(".particles"); // SelectorAll seleciona todas as divs de paticles
+
+particlesContainers.forEach(container => {
+
+    for (let i = 0; i < 30; i++) {
+        const span = document.createElement("span");
+
+        span.style.left = Math.random() * 100 + "%";
+        span.style.animationDuration = (5 + Math.random() * 5) + "s";
+        span.style.opacity = Math.random();
+
+        container.appendChild(span);
+    }
+
+});
+
+
+
+
+
+
